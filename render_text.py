5a2f900dbc42d31e58d8408ce9e4f2685519681b
Esteban Rey <erey@stanford.edu>
2019-05-23 00:48:38 -0700

    Edging closer to bitmap reading

--- render_text.py
+++ render_text.py
@@ -2,32 +2,33 @@ from PIL import Image
 
 
 def render_alpha_num(char, ser):
+    grid = [['0' for i in range(8)] for j in range(8)]
     my_bmp = Image.open('font.png')
     data = my_bmp.getdata()
-    location_x = ord(char) % 256
-    location_y = (ord(char) // 256)*8
+    location_x = (ord(char[0]) % 16) *32
+    location_y = (ord(char[0]) % 16) *32
 
     pixels = my_bmp.load() # this is not a list, nor is it list()'able
     width, height = my_bmp.size
     #This is to commit
-
-    for x in range(location_x, location_x + 32):
-        for y in range(location_y, location_y + 32):
+    arr_y = 0
+    for y in range(location_y, location_y + 32):
+        arr_x = 0
+        for x in range(location_x, location_x + 32):
             cpixel = pixels[x, y]
             if (cpixel == (255, 255, 255)):
-                print ("", end =" ")
+                grid[arr_x//4][arr_y//4] = 1
             else:
-                print ("0", end =" ")
+                grid[arr_x//4][arr_y//4] = 0
+            arr_x += 1
+        arr_y += 1
+    
+    for arr in grid:
+        for elem in arr:
+            print(elem, end ="")
+        print()
 
-    # for i,p in enumerate(data):
-    #     if (i % 512 == 0): print ()
-    #     if (p == (255, 255, 255)):
-    #         print ("", end =" ")
-    #     else:
-    #         print (0, end ="")
-	#ser.write(grid_str.encode())
-
-render_alpha_num("c", "c");
+render_alpha_num("0", "c")
 
 #def output_string(str, ser):
 	#for char in str:
